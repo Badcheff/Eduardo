ce688758dcda7d5c1906dfed2df801ed25a3d324
Badcheff <92431239+Badcheff@users.noreply.github.com>
2025-02-19 14:59:36 +0000

    Edt test
    test

---
+++ Tabuadas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual tabuada queres saber?");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i+1}*" + num + "=" + (i+1) * num);
+            }
+        }
+    }
+}
